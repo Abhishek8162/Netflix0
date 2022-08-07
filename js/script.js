de91c1bad92e68a9243500a8fb6d8bad32924f2c
Abhishek8162 <83363682+Abhishek8162@users.noreply.github.com>
2022-08-07 13:12:58 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var c=0;
+function chng()
+{c++;
+    if(c%2!=0)
+    document.getElementById("d1").innerHTML="SEE THIS IS NOW CHANGED";
+   else
+   document.getElementById("d1").innerHTML="So this is the place which would get invisible";
+}
+console.log("hi there");
